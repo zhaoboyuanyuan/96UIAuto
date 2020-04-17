7d7d5a7ccaa0150438f45a92c787ed11de9fdff6
fubisi <1309817607@qq.com>
2020-04-17 17:09:53 +0800

    修改

---
+++ public/data.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+#  creaded by 赵永健
+
+phoneNumber="15951644332"
+pass1="123456abcd"
+
+urlFirst='https://www.51safety.com.cn/'
+urlSecond='http://www.proj.lets.aysaas.com:23121/'
+
+# userName='admin_tcxt'
+# password='111111'
+
+#账户
+userName='admin_txjy'
+#密码
+password='111111'
+#主url
+murl='taixingjinyanhuaxuea'
+
+#需手动新建测试账号托管
